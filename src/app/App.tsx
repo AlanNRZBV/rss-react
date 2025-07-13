6cff47fdde9b2139daeee99c54a59f085d859c89
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2025-07-13 16:26:57 +0600

    add ItemList feature

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import { Component } from 'react';
 import SearchBar from '../features/SearchBar/SearchBar.tsx';
+import ItemList from '../features/ItemList/ItemList.tsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="border border-amber-600 h-full w-full">
-        <SearchBar />
+      <div className="border border-amber-600 h-full w-full flex justify-center">
+        <div className="border border-blue-500 w-full mx-4 2xl:mx-32 xl:mx-28 lg:mx-16 sm:mx-8">
+          <SearchBar />
+          <ItemList />
+        </div>
       </div>
     );
   }
---
+++ src/features/ItemList/ItemList.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class ItemList extends Component {
+  render() {
+    return <div>cards here</div>;
+  }
+}
+
+export default ItemList;
