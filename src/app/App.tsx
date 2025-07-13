5035d1208d34b219df57762976452865bc7456cc
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2025-07-13 16:15:58 +0600

    add SearchBar feature

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
 import { Component } from 'react';
+import SearchBar from '../features/SearchBar/SearchBar.tsx';
 
 class App extends Component {
   render() {
-    return <div className="uppercase">Test message</div>;
+    return (
+      <div className="border border-amber-600 h-full w-full">
+        <SearchBar />
+      </div>
+    );
   }
 }
 export default App;
---
+++ src/features/SearchBar/SearchBar.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return <div>searchbar here</div>;
+  }
+}
+
+export default SearchBar;
