0269ad076135519701d2a15752b81da4396d5f61
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2025-07-20 14:14:46 +0600

    feat: implement search component user interaction test

--- src/features/SearchBar/SearchBar.test.tsx
+++ src/features/SearchBar/SearchBar.test.tsx
@@ -2,9 +2,14 @@ import { beforeEach, describe, expect, test } from 'vitest';
 import SearchBar from './SearchBar.tsx';
 import { render, screen } from '@testing-library/react';
 import { vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+import { type ChangeEvent, type FormEvent } from 'react';
 
 describe('SearchBar', () => {
+  const key = 'searchData';
+  const value = 'testString';
+
   describe('render UI elements', () => {
     beforeEach(() => {
       render(
@@ -32,13 +37,9 @@ describe('SearchBar', () => {
       localStorage.clear();
     });
 
-    const key = 'searchData';
-    const value = 'testString';
     test('should display data saved in local storage', () => {
       localStorage.setItem(key, value);
-
       const dataFromLS = localStorage.getItem(key);
-
       render(
         <SearchBar
           search={dataFromLS ? dataFromLS : ''}
@@ -51,9 +52,9 @@ describe('SearchBar', () => {
       const input = screen.getByLabelText(/search/i);
       expect(input).toHaveValue(dataFromLS);
     });
+
     test('should display empty input when no data in local storage', () => {
       const dataFromLS = localStorage.getItem(key);
-
       const value = dataFromLS ? dataFromLS : '';
 
       render(
@@ -67,6 +68,122 @@ describe('SearchBar', () => {
 
       const input = screen.getByLabelText(/search/i);
       expect(input).toHaveValue(value);
+    });
+  });
+
+  describe('user interaction', () => {
+    let searchValue = '';
+    const user = userEvent.setup();
+
+    beforeEach(() => {
+      searchValue = '';
+      localStorage.clear();
+    });
+
+    const handleMockChange = vi.fn((e: ChangeEvent<HTMLInputElement>) => {
+      searchValue = e.target.value.trim();
+      localStorage.setItem(key, searchValue);
+    });
+
+    const handleMockSubmit = vi.fn((e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+    });
+
+    test('should update input value on user typing', async () => {
+      const { rerender } = render(
+        <SearchBar
+          search={searchValue}
+          onChange={handleMockChange}
+          onSubmit={vi.fn()}
+          isLoading={false}
+        />
+      );
+      const input = screen.getByLabelText(/search/i);
+
+      for (const char of value) {
+        await user.type(input, char);
+        rerender(
+          <SearchBar
+            search={searchValue}
+            onChange={handleMockChange}
+            onSubmit={vi.fn()}
+            isLoading={false}
+          />
+        );
+      }
+      expect(input).toHaveValue(value);
+    });
+
+    test('should save input value into local storage', async () => {
+      const { rerender } = render(
+        <SearchBar
+          search={searchValue}
+          onChange={handleMockChange}
+          onSubmit={vi.fn()}
+          isLoading={false}
+        />
+      );
+      const input = screen.getByLabelText(/search/i);
+
+      for (const char of value) {
+        await user.type(input, char);
+        rerender(
+          <SearchBar
+            search={searchValue}
+            onChange={handleMockChange}
+            onSubmit={vi.fn()}
+            isLoading={false}
+          />
+        );
+      }
+      const dataFromLS = localStorage.getItem(key);
+
+      expect(dataFromLS).toBe(searchValue);
+    });
+
+    test('trims whitespace from search input before saving', async () => {
+      searchValue = '';
+
+      const { rerender } = render(
+        <SearchBar
+          search={searchValue}
+          onChange={handleMockChange}
+          onSubmit={vi.fn()}
+          isLoading={false}
+        />
+      );
+
+      const input = screen.getByLabelText(/search/i);
+      const whiteSpaceValue = '   testString   ';
+
+      for (const char of whiteSpaceValue) {
+        await user.type(input, char);
+        rerender(
+          <SearchBar
+            search={searchValue}
+            onChange={handleMockChange}
+            onSubmit={vi.fn()}
+            isLoading={false}
+          />
+        );
+      }
+
+      const saved = localStorage.getItem(key);
+      expect(saved).toBe(value);
+    });
+    test('should trigger search callback with correct parameters', async () => {
+      render(
+        <SearchBar
+          search={'bulbasaur'}
+          onChange={handleMockChange}
+          onSubmit={handleMockSubmit}
+          isLoading={false}
+        />
+      );
+      const btn = screen.getByRole('button', { name: /submit/i });
+      await user.click(btn);
+      expect(handleMockSubmit).toHaveBeenCalledTimes(1);
     });
   });
 });
