2caad772d3e3d662bfd3d9fef4f2e5dd0a3f1717
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2025-07-26 23:37:50 +0600

    ref: convert PokemonsListItem to functional component

--- src/features/PokemonsList/PokemonsListItem.tsx
+++ src/features/PokemonsList/PokemonsListItem.tsx
@@ -1,21 +1,19 @@
-import { Component } from 'react';
+import type { Pokemon } from '../../types';
+import type { FC } from 'react';
 
-class PokemonsListItem extends Component<Pokemon> {
-  render() {
-    const { name, url } = this.props;
-    return (
-      <tr className="not-last:border not-last:border-b-gray-400 not-even:bg-gray-100">
-        <td className="flex justify-center px-2 py-1">
-          <span className="font-medium capitalize">{name}</span>
-        </td>
-        <td>
-          <a href={url} className="text-blue-400">
-            {url}
-          </a>
-        </td>
-      </tr>
-    );
-  }
-}
+const PokemonsListItem: FC<Pokemon> = ({ name, url }) => {
+  return (
+    <tr className="not-last:border not-last:border-b-gray-400 not-even:bg-gray-100">
+      <td className="flex justify-center px-2 py-1">
+        <span className="font-medium capitalize">{name}</span>
+      </td>
+      <td>
+        <a href={url} className="text-blue-400">
+          {url}
+        </a>
+      </td>
+    </tr>
+  );
+};
 
 export default PokemonsListItem;
